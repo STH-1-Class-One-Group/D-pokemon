dd4bc9db476e186628fb3ce15d9f8179e265ecef
user <nohyunkim3@gmail.com>
2026-02-24 16:18:38 +0900

    fix: App.tsx 머지 충돌 해결 및 라우터 구조 유지

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-<<<<<<< HEAD
-// src/App.tsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import Home from './pages/Home';
 import Pokedex from './pages/Pokedex';
 import Quiz from './pages/Quiz';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* 모든 페이지에 공통 상단바/하단바를 적용하기 위해 Layout으로 감쌉니다 */}
-          <Route element={<Layout />}>
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/pokedex" element={<Pokedex />} />
           <Route path="/quiz" element={<Quiz />} />
@@ -22,20 +21,3 @@ function App() {
 }
 
 export default App;
-=======
-// src/App.tsx (임시 테스트용)
-import { useEffect } from 'react';
-import { getPokemonList } from './api/pokemonapi';
-
-function App() {
-  useEffect(() => {
-    const testFetch = async () => {
-      const data = await getPokemonList(20, 0);
-      console.log("🔥 API 연결 성공! 포켓몬 20마리:", data);
-    };
-    testFetch();
-  }, []);
-
-  return <div>배포 및 API 테스트 중...</div>;
-}
->>>>>>> origin/feature/core-api-mk
