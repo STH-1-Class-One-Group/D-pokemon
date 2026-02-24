ddb0b63eccde63b54975ddc6110681fb4aa896b4
신재혁 <tlswogur0603@gmail.com>
2026-02-24 14:37:32 +0900

    fix: CSS 클래스 우선순위 문제 해결 및 pokedex-container 추가

--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,10 +1,13 @@
-// pages/Pokedex.tsx
 import React from 'react';
 import PokemonList from '../components/pokedex/PokemonList';
 
 const Pokedex = () => {
     return (
-        <div className="pokedex-page">
+        /* 아래 div에 'pokedex-container' 클래스를 추가했습니다. 
+            이제 CSS에서 .pokedex-container h1 { ... } 로 접근하여 
+            디자인을 확실하게 바꿀 수 있습니다. 
+        */
+        <div className="pokedex-container">
             <h1>포켓몬 도감</h1>
             <PokemonList />
         </div>
