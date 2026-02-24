a5842aca2292787c5c69b3c0cfdf494461f48af0
user <nohyunkim3@gmail.com>
2026-02-24 15:09:35 +0900

    feat: 상/하단 내비게이션(Header, BottomNav) 디자인 및 라우터 링크 연결

--- src/components/common/BottomNav.tsx
+++ src/components/common/BottomNav.tsx
@@ -1,14 +1,19 @@
-// src/components/common/BottomNav.tsx
+import { Link } from 'react-router-dom'; // 링크 기능 불러오기
 import './BottomNav.css';
 
 export default function BottomNav() {
     return (
-        // style={...} 삭제하고 className 사용!
         <nav className="bottom-nav">
         <ul>
-            <li>홈</li>
-            <li>도감</li>
-            <li>퀴즈</li>
+            <li>
+            <Link to="/">홈</Link>
+            </li>
+            <li>
+            <Link to="/pokedex">도감</Link>
+            </li>
+            <li>
+            <Link to="/quiz">퀴즈</Link>
+            </li>
         </ul>
         </nav>
     );
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,12 +1,22 @@
-// src/components/common/Header.tsx
+import { Link } from 'react-router-dom'; // 링크 이동 기능
 import './Header.css';
 
-    export default function Header() {
+export default function Header() {
     return (
-        // style={...} 삭제하고 className 사용!
         <header className="header">
-        <h1>D-Pokemon</h1>
-        {/* 메뉴가 필요하면 여기에 추가 */}
+        {/* 왼쪽: 로고/제목 */}
+        <h1 className="header-title">
+            <Link to="/">D-Pokemon</Link>
+        </h1>
+
+        {/* 오른쪽: PC 전용 메뉴 (와이어프레임처럼) */}
+        <nav className="header-menu">
+            <Link to="/">Home</Link>
+            <span className="divider">|</span>
+            <Link to="/pokedex">Pokedex</Link>
+            <span className="divider">|</span>
+            <Link to="/quiz">Quiz</Link>
+        </nav>
         </header>
     );
 }
