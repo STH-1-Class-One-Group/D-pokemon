27dc7cca447b0470df7e59c16409f6afd9c3e523
user <nohyunkim3@gmail.com>
2026-02-26 12:40:08 +0900

    카드 컴포넌트에 넘겨줄 id props 코드 추가

--- src/components/pokedex/PokemonList.tsx
+++ src/components/pokedex/PokemonList.tsx
@@ -10,13 +10,13 @@ function PokemonList({ list }: PokemonListProps) {
     return (
         <div className="pokemon-list">
             {list.map((pokemon) => (
-    <PokemonCard 
-        key={pokemon.id}
-        name={pokemon.name}
-        url={pokemon.url}
-        // 👇 바로 이 줄을 추가해야 최종적으로 카드에 한글 이름이 전달됩니다!
-        korean_name={pokemon.korean_name} 
-    />
+                <PokemonCard 
+                    key={pokemon.id}       // (1) 리액트 내부 식별용 key
+                    id={pokemon.id}        // (2) 카드 컴포넌트에 넘겨줄 id props
+                    name={pokemon.name}
+                    url={pokemon.url}
+                    korean_name={pokemon.korean_name} 
+                />
 ))}
         </div>
     );
